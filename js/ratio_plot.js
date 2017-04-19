4df6f0cb9fd89151274e17e0572535b9fd533646
Matteo De Felice <matteo.defelice@gmail.com>
2017-04-19 16:02:00 +0200

    Removed April 2017
    Due to unsufficient data.

--- js/ratio_plot.js
+++ js/ratio_plot.js
@@ -2,7 +2,7 @@
     $(document).ready(function() {
 	
 	$('#ratio_plot').scianimator({
-	    'images': ['images/ratio_plot1.png', 'images/ratio_plot2.png', 'images/ratio_plot3.png', 'images/ratio_plot4.png', 'images/ratio_plot5.png', 'images/ratio_plot6.png', 'images/ratio_plot7.png', 'images/ratio_plot8.png', 'images/ratio_plot9.png', 'images/ratio_plot10.png', 'images/ratio_plot11.png', 'images/ratio_plot12.png', 'images/ratio_plot13.png', 'images/ratio_plot14.png', 'images/ratio_plot15.png', 'images/ratio_plot16.png', 'images/ratio_plot17.png', 'images/ratio_plot18.png', 'images/ratio_plot19.png', 'images/ratio_plot20.png', 'images/ratio_plot21.png', 'images/ratio_plot22.png', 'images/ratio_plot23.png', 'images/ratio_plot24.png', 'images/ratio_plot25.png', 'images/ratio_plot26.png', 'images/ratio_plot27.png', 'images/ratio_plot28.png'],
+	    'images': ['images/ratio_plot1.png', 'images/ratio_plot2.png', 'images/ratio_plot3.png', 'images/ratio_plot4.png', 'images/ratio_plot5.png', 'images/ratio_plot6.png', 'images/ratio_plot7.png', 'images/ratio_plot8.png', 'images/ratio_plot9.png', 'images/ratio_plot10.png', 'images/ratio_plot11.png', 'images/ratio_plot12.png', 'images/ratio_plot13.png', 'images/ratio_plot14.png', 'images/ratio_plot15.png', 'images/ratio_plot16.png', 'images/ratio_plot17.png', 'images/ratio_plot18.png', 'images/ratio_plot19.png', 'images/ratio_plot20.png', 'images/ratio_plot21.png', 'images/ratio_plot22.png', 'images/ratio_plot23.png', 'images/ratio_plot24.png', 'images/ratio_plot25.png', 'images/ratio_plot26.png', 'images/ratio_plot27.png'],
 	    'width': 1200,
 	    'delay': 1000,
 	    'loopMode': 'loop'
